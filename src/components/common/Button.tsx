b393beb3855851e1926b14d097e66d39cb0a85ce
southxzx <phuongnamsk109@gmail.com>
2023-06-20 17:57:02 +0700

    add variant to Button, add script for About screen

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -6,6 +6,7 @@ import LoadingIcon from "./Loading";
 type IButtonProps = {
   disabled?: boolean;
   loading?: boolean;
+  variant?: "primary" | "secondary";
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<IButtonProps> = ({
@@ -14,13 +15,16 @@ const Button: React.FC<IButtonProps> = ({
   loading,
   className,
   style,
+  variant = "primary",
   ...rest
 }) => {
   const _disabled = disabled || loading;
   return (
     <button
       disabled={_disabled}
-      className={`${className} btn ${_disabled ? "btn-disabled" : ""}`}
+      className={`${className} btn-${variant} btn ${
+        _disabled ? "btn-disabled" : ""
+      }`}
       style={style}
       onClick={!_disabled ? rest.onClick : undefined}
       {...rest}
--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -22,8 +22,41 @@ const AboutScreen: React.FC<IAboutScreenProps> = ({ api }) => {
     );
   };
   return (
-    <div>
-      <Button onClick={onLogout}>Logout</Button>
+    <div className="screen-container about-screen">
+      <div className="info">
+        <p>
+          This plugin helps you guys faster the process of filling copies, now
+          you can say good bye to the “Lorem Ipsum” Legendary.
+        </p>
+        <p>
+          <span>NOTE: </span>To use this, you need to have{" "}
+          <a
+            href="https://platform.openai.com/account/usage"
+            target="_blank"
+            rel="nofollow"
+          >
+            available credit
+          </a>{" "}
+          in your OpenAI Account.
+        </p>
+      </div>
+      <div className="bottom-block">
+        <div className="left-bottom-block">
+          <a href="mailto:example@example.com" target="_blank">
+            Get Support
+          </a>
+          <a
+            href="https://docs.google.com/forms/d/e/1FAIpQLSflzQh3ODnxBB6xxHyRRtl_N3tafGDLIvAwv9n0mpKFG-TjAQ/viewform?usp=sf_link"
+            target="_blank"
+            rel="nofollow"
+          >
+            Report Bug
+          </a>
+        </div>
+        <Button onClick={onLogout} variant="secondary">
+          Log Out
+        </Button>
+      </div>
     </div>
   );
 };
